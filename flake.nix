c00088f2cb68962b85045dc0b1a3aa960c8f3e1f
Dominik Schrempf <dominik.schrempf@gmail.com>
2025-05-06 23:18:32 +0200

    ghc98, and upstream hvega (seems to work fine now)

--- flake.nix
+++ flake.nix
@@ -3,41 +3,39 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.hvega.url = "github:dschrempf/hvega/dom";
-  inputs.hvega.inputs.nixpkgs.follows = "nixpkgs";
-
   inputs.mcmc.url = "github:dschrempf/mcmc";
   inputs.mcmc.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { self
-    , flake-utils
-    , hvega
-    , mcmc
-    , nixpkgs
+    {
+      self,
+      flake-utils,
+      mcmc,
+      nixpkgs,
     }:
     let
       theseHpkgNames = [
         "webapp"
       ];
-      thisGhcVersion = "ghc96";
+      thisGhcVersion = "ghc98";
       hOverlay = selfn: supern: {
         haskell = supern.haskell // {
-          packageOverrides = selfh: superh:
-            supern.haskell.packageOverrides selfh superh //
-              {
-                webapp = selfh.callCabal2nix "webapp" ./. { };
-              };
+          packageOverrides =
+            selfh: superh:
+            supern.haskell.packageOverrides selfh superh
+            // {
+              webapp = selfh.callCabal2nix "webapp" ./. { };
+            };
         };
       };
       overlays = [
         hOverlay
-        hvega.overlays.default
         mcmc.overlays.default
       ];
-      perSystem = system:
+      perSystem =
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -46,13 +44,12 @@
           hpkgs = pkgs.haskell.packages.${thisGhcVersion};
           hlib = pkgs.haskell.lib;
           theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hlib.dontCheck hpkgs.${n});
-          theseHpkgsDev = builtins.mapAttrs
-            (_: x: hlib.doCheck
-              (hlib.doBenchmark x))
-            theseHpkgs;
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doCheck (hlib.doBenchmark x)) theseHpkgs;
         in
         {
-          packages = theseHpkgs // { default = theseHpkgs.webapp; };
+          packages = theseHpkgs // {
+            default = theseHpkgs.webapp;
+          };
 
           devShells.default = hpkgs.shellFor {
             packages = _: (builtins.attrValues theseHpkgsDev);
@@ -71,7 +68,10 @@
     in
     {
       overlays.default = nixpkgs.lib.composeManyExtensions overlays;
-      nixosModules = let webapp = import ./modules/webapp.nix self; in
+      nixosModules =
+        let
+          webapp = import ./modules/webapp.nix self;
+        in
         {
           inherit webapp;
           default = webapp;
